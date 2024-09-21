4bc2f12138557262b522d91dd752c713bdbfbe7c
Evandro Calado <evandrocalado07@gmail.com>
2024-09-21 00:04:17 -0300

    🐳 chore: add tasks

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,17 +1,40 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Button } from "../Button";
+import { Task, TaskProps } from "../Task";
 import "./Board.scss";
 
 export type BoardProps = {
-  children?: React.ReactNode;
+  initialTasks?: TaskProps[];
 };
 
-export const Board: FC<BoardProps> = ({ children }) => {
+export const Board: FC<BoardProps> = ({ initialTasks }) => {
+  const [tasks, setTasks] = useState<TaskProps[]>(initialTasks || []);
+
+  const tasksNotFinished = tasks.filter((task) => !task.finished);
+  const tasksFinished = tasks.filter((task) => task.finished);
+
   return (
     <section className="board">
-      <h2 className="board-title">Suas tarefas de hoje</h2>
+      <div className="board-content">
+        <h2 className="board-content-title">Suas tarefas de hoje</h2>
+        {tasksNotFinished && tasksNotFinished.length === 0 ? (
+          <p className="board-content-no-tasks">Nenhuma tarefa encontrada</p>
+        ) : (
+          tasksNotFinished.map((task) => <Task key={task.id} {...task} />)
+        )}
 
-      {children}
+        {tasksFinished && (
+          <>
+            <h2 className="board-content-title">Tarefas finalizadas</h2>
+            {tasksFinished.map((task) => (
+              <Task key={task.id} {...task} finished />
+            ))}
+          </>
+        )}
+      </div>
+
       <Button>Adcionar nova tarefa</Button>
     </section>
   );
