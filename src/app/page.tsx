053360d25fcc5a1d82e9a9f4f1592e25ea3be52a
Evandro Calado <evandrocalado07@gmail.com>
2024-09-21 00:03:33 -0300

    🐳 chore: add tasks, header and board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { Board, Header } from "@/components";
+
+const initialTasks = [
+  {
+    id: 1,
+    task: "Lavar as maos",
+  },
+  {
+    id: 2,
+    task: "fazer um bolo",
+  },
+  {
+    id: 3,
+    task: "Lavar a louça",
+  },
+  {
+    id: 4,
+    task: "Levar o lixo para fora",
+    finished: true,
+  },
+];
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <Header />
+      <Board initialTasks={initialTasks} />
     </main>
   );
 }
