cf17e549067be10945c0b3e72480c6652a7416ee
Evandro Calado <evandrocalado07@gmail.com>
2024-09-21 21:22:54 -0300

    🐳 chore: add logic and styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,45 @@
+"use client";
+
 import { FC } from "react";
 import { Button, ButtonVariant } from "../Button";
+import { TaskProps } from "../Task";
 import "./Modal.scss";
 
 export type ModalProps = {
   title: string;
   buttonVariant: ButtonVariant;
+  isOpen?: boolean;
+  setIsOpen?: (task: TaskProps | null) => void;
+  onClick?: () => void;
+  onAddTask?: (title: string) => void;
 };
 
-export const Modal: FC<ModalProps> = ({ title, buttonVariant = "primary" }) => {
+export const Modal: FC<ModalProps> = ({
+  title,
+  buttonVariant = "primary",
+  isOpen,
+  setIsOpen,
+  onClick,
+  onAddTask,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onAddTask?.(event.target.value);
+  };
+
   return (
-    <div className="modal">
+    <div className={`modal ${isOpen ? "open" : ""}`}>
       <div className="modal-content">
         <h2 className="modal-content-title">{title}</h2>
 
         {buttonVariant === "primary" && (
           <div className="modal-content-add">
             <label htmlFor="add">Título</label>
-            <input id="add" type="text" placeholder="Digite" />
+            <input
+              id="add"
+              type="text"
+              placeholder="Digite"
+              onChange={handleChange}
+            />
           </div>
         )}
 
@@ -27,8 +50,10 @@ export const Modal: FC<ModalProps> = ({ title, buttonVariant = "primary" }) => {
         )}
 
         <div className="modal-content-buttons">
-          <Button variant="ghost">Cancelar</Button>
-          <Button variant={buttonVariant}>
+          <Button variant="ghost" onClick={() => setIsOpen?.(null)}>
+            Cancelar
+          </Button>
+          <Button variant={buttonVariant} onClick={onClick}>
             {buttonVariant === "primary" ? "Adicionar" : "Deletar"}
           </Button>
         </div>
